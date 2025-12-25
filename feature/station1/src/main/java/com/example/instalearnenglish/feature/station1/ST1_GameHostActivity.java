174e2683ef6f54e7fe9e2a1f9877c6d0ce140290
BaoAn205 <23162003@student.hcmute.edu.vn>
2025-12-25 23:51:40 +0700

    thêm tổng 7 game cho tab game của trạm 1

--- feature/station1/src/main/java/com/example/instalearnenglish/feature/station1/ST1_GameHostActivity.java
+++ feature/station1/src/main/java/com/example/instalearnenglish/feature/station1/ST1_GameHostActivity.java
@@ -7,33 +7,40 @@ import androidx.fragment.app.Fragment;
 public class ST1_GameHostActivity extends AppCompatActivity {
 
     public static final String EXTRA_GAME_TYPE = "EXTRA_GAME_TYPE";
-    public static final String EXTRA_GAME_TITLE = "EXTRA_GAME_TITLE"; // nếu muốn hiển thị title
+    public static final String EXTRA_GAME_TITLE = "EXTRA_GAME_TITLE"; 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.st1_activity_game);  // ← chính là file XML bạn vừa gửi
+        setContentView(R.layout.st1_activity_game);
 
-        if (savedInstanceState == null) {  // chỉ add fragment lần đầu
+        if (savedInstanceState == null) {
             String gameType = getIntent().getStringExtra(EXTRA_GAME_TYPE);
 
             Fragment fragment = null;
             if ("DRAG_AND_DROP_LUGGAGE".equals(gameType)) {
                 fragment = new ST1_DragAndDropLuggageFragment();
-                // Nếu muốn truyền title hoặc data khác
-                // Bundle args = new Bundle();
-                // args.putString("GAME_TITLE", getIntent().getStringExtra(EXTRA_GAME_TITLE));
-                // fragment.setArguments(args);
+            } else if ("QUIZ_PROHIBITED_ITEMS".equals(gameType)) {
+                fragment = new ST1_QuizGameFragment();
+            } else if ("GUESS_MY_TRIP".equals(gameType)) {
+                fragment = new ST1_GuessTripGameFragment();
+            } else if ("EMOJI_PACKING".equals(gameType)) {
+                fragment = new ST1_EmojiPackingGameFragment();
+            } else if ("WHATS_IN_MY_BAG".equals(gameType)) {
+                fragment = new ST1_WhatsInMyBagGameFragment();
+            } else if ("FORGOT_SOMETHING".equals(gameType)) {
+                fragment = new ST1_ForgotSomethingGameFragment();
+            } else if ("WORD_IMAGE_MATCH".equals(gameType)) {
+                fragment = new ST1_WordImageMatchGameFragment();
             }
-            // Sau này thêm các game khác ở đây: else if ("OTHER_GAME"...
-
+            
             if (fragment != null) {
                 getSupportFragmentManager()
                         .beginTransaction()
-                        .replace(R.id.game_fragment_container, fragment)  // ← ID trong XML của bạn
+                        .replace(R.id.game_fragment_container, fragment)
                         .commit();
             } else {
-                finish(); // không có game nào hợp lệ
+                finish(); 
             }
         }
     }
